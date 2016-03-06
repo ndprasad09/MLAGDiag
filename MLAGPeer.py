657726f7b5da0f1f2964d126f212a78aeaa195f8
rameshvp09 <rameshvp09@gmail.com>
2016-03-06 11:35:13 +0530

    corrected spelling mistakes and removed print statement

--- MLAGPeer.py
+++ MLAGPeer.py
@@ -5,7 +5,7 @@ import Library
 import re
 def get_mlag_peer(handler,SwitchID):
     """
-    This procedure collects MLAG Peer Informations
+    This procedure collects MLAG Peer Information
     and inserts in to the table MLAGPeerInstance
 
     :param handler: connection handler to the switch
@@ -25,13 +25,13 @@ def get_mlag_peer(handler,SwitchID):
     ISCVlanID = 0
     ISCId = 0
     index = 0
-    #-- Disable cli paging in swictch
+    #-- Disable cli paging in switch
     Library.SendCmd(handler,"disable clipaging")
     #-- Get the show mlag peer output
     wholeoutput = Library.SendCmd(handler,"show mlag peer")
 
     #-- Parse the output for required fields to feed to MLAGPeer table
-    #-- Peer Name
+    #-- Peer Name list
     PeerNameexistlist = re.findall(r"MLAG\s+Peer\s+:\s+(\w+)\s+.*",wholeoutput)
     if PeerNameexistlist:
         logging.info("MLAG Peers are present")
@@ -39,24 +39,29 @@ def get_mlag_peer(handler,SwitchID):
         logging.error("No MLAG Peer Exists in Switch ID : "+str(SwitchID))
         logging.error("Returning with failure to update the MLAG Peer Instance")
         return
-    #-- split the outpur MLAG peerwise
+
+    #-- split the output MLAG peerwise
     outputpart = Library.split_before("MLAG\s+Peer\s+:",wholeoutput)
 
+    #-- Parse the output peerwise
     for output in list(outputpart):
+
         #-- Peer Name
         PeerName = re.search(r"MLAG\s+Peer\s+:\s+(\w+)\s+.*",output)
         if PeerName == None:
             continue
         PeerName=PeerName.groups(1)[index]
-        print PeerName
+
         #-- Virtual router
         VirtualRouterexist = re.search (r"Virtual\s+Router\s+:\s+(.*)\s+\r",output)
         if VirtualRouterexist:
             VirtualRouter = VirtualRouterexist.groups(1)[index]
+
         #-- Checkpointstatus
         CheckpointStatusexist = re.search (r"Checkpoint\s+Status\s+:\s+(Up|Down)\s+.*",output)
         if CheckpointStatusexist:
             CheckpointStatus=CheckpointStatusexist.groups(1)[index]
+
         #-- Authentication
         Authenticationexist = re.search(r"Authentication\s+:\s+(\w+)\s+.*",output)
         if Authenticationexist:
@@ -85,8 +90,10 @@ def get_mlag_peer(handler,SwitchID):
                 PeerIPAddress = PIpAddressexist.groups(1)[0]
             else:
                 logging.info("Peer IP addrees not configured for peer:"+PeerName)
+
             #-- Get show isc vlan output from switch
             output2 = Library.SendCmd(handler,"show vlan "+ISCVlan)
+
             #-- ISC vlan specific information
             #-- Tag status
             TagInfo = re.search (r"Tagging:(.*)",output2)
@@ -119,5 +126,6 @@ def get_mlag_peer(handler,SwitchID):
                 ISCPort = ISCPort.strip('g')
         else:
             logging.error("No ISCVlan present !!!")
+
         #-- Add to Database
         MLAGSQL.AddMLAGPeerInstance(SwitchID,ISCId,ISCPort,PeerName,VirtualRouter,PeerIPAddress,ISCVlan,LocalIPAddress,ISCVlanID,CheckpointStatus,Authentication,MLAGPorts)
