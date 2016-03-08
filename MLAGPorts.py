40485140d1c0746ddce65548c71174ca304e0319
Srinivas P G <srinivas11789@gmail.com>
2016-03-08 16:39:35 +0530

    Update with ISC Port Fetching
    * ISC Port details Fetching
    * Peer Name get addition
    * ISC Port, ISC ID get addition

--- MLAGPorts.py
+++ MLAGPorts.py
@@ -19,20 +19,26 @@ def MlagPort(handle, SwitchID):
     mlag_port = Library.SendCmd(handle, "show mlag ports")
     port_id = []
     mlag_id = []
+    mlag_peer = []
     lines = mlag_port.splitlines()
     for x in lines:
-        mlag_info = re.search('^([0-9]+)\s+([0-9]+(:[0-9]+|(?!:)))\s*', x)
+        mlag_info = re.search('^([0-9]+)\s+([0-9]+(:[0-9]+|(?!:)))\s+(A|R|D|NP)\s+(Up|Down|N\/A)\s+([A-Za-z][A-Za-z0-9]+)\s+', x)
         if mlag_info:
             if mlag_info.group(1):
                 mlag_id.append(mlag_info.group(1))
             if mlag_info.group(2):
                 port_id.append(mlag_info.group(2))
-
+            if mlag_info.group(6):
+                mlag_peer.append(mlag_info.group(6))
+                
 
     ### Show ports details for every port to capture the associated Vlan information
-    for index1,index2 in zip(port_id,mlag_id):
+    for index1,index2,index3 in zip(port_id,mlag_id,mlag_peer):
         port_information = Library.SendCmd(handle, "show ports %s information detail" % (index1))
+        ISC_id = MLAGSQL.c.execute("SELECT ISCID from MLAGPeer where SwitchID = '%s' and MLAGPeerName = '%s'" % (SwitchID, index3))
+        ISCList = ISC_id.fetchall()
         Port_Vlan_info = re.findall('Name:.(.+?),.*Tag =.(.+?),.*\n\s+(Port.*:.*[0-9]*|(?!Port))', port_information)
+        ISCList[0] = list(ISCList[0])
         if Port_Vlan_info:
             for index in Port_Vlan_info:
                         index = list(index)
@@ -40,10 +46,23 @@ def MlagPort(handle, SwitchID):
                                index[2] = 1
                         else:
                                index[2] = 0
-                        print "switch,isc,%s,%s,%s,%s,%s" % (index1, index2, index[0], index[1], index[2])
+                        
     ### AddPortInfo(SwitchID, ISC_id, Port_id, Mlag_id, VlanName, VlanTag, Tag|Untag)
-                        AddPortInfo(SwitchID, ISC_id, index1, index2, index[0], index[1], index[2])
+                        MLAGSQL.AddPortInfo(SwitchID, ISCList[0][0], index1, index2, index[0], index[1], index[2])
 
-### Note : Need to work for a solution for the ISC ID fetching and Error Handle
-                                        
 
+    for peer in mlag_peer:
+        ISC = MLAGSQL.c.execute("SELECT ISCPort,ISCID from MLAGPeer where SwitchID = '%s' and MLAGPeerName = '%s'" % (SwitchID, peer))
+        ISCDetail = ISC_id.fetchall()
+        ISC_Port_Vlan_output = Library.SendCmd(handle, "show ports %s information detail" % (ISCDetail[0][0]))
+        ISC_Port_Vlan_info = re.findall('Name:.(.+?),.*Tag =.(.+?),.*\n\s+(Port.*:.*[0-9]*|(?!Port))', ISC_Port_Vlan_output)
+        if ISC_Port_Vlan_info:
+            for index in ISC_Port_Vlan_info:
+                        index = list(index)
+                        if "Port" in index[2]:
+                               index[2] = 1
+                        else:
+                               index[2] = 0
+        
+                        MLAGSQL.AddPortInfo(SwitchID, ISCDetail[0][1], ISCDetail[0][0], 0, index[0], index[1], index[2])
+        
