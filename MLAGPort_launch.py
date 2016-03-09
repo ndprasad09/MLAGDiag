ccb7d9bd7a68386f7f5341c5a22bc88ab3d8ee65
Srinivas P G <srinivas11789@gmail.com>
2016-03-09 16:34:39 +0530

    Creating Launch file for MLAGPorts
    Launch file for MLAG Ports

---
+++ MLAGPort_launch.py
@@ -0,0 +1,73 @@
+import MLAGSQL
+import logging
+import connect
+import MLAGPeer
+import MLAGPorts
+# The default level is WARNING(30). The available levels are INFO(20),DEBUG(10),ERROR(40) and CRITICAL(50)
+# Use the approppriate log levels for debug print outputs
+logging.basicConfig(level=logging.INFO)
+
+# Connect to the Database
+MLAGSQL.ConnectDatabase()
+
+#Create the tables required for analysis
+MLAGSQL.CreateTables()
+
+# Add the following Procs
+"""
+AddMlagPeerInstance
+
+#(SwitchID integer,ISCID integer,ISCPort integer,
+# MLAGPeerName text,VRName text,PeerIPAddress text,
+# ISCVlanName text,ISCIP text,ISCVlanTag int,ChkPtStatus int,
+# AuthMethod text,NumMLAGPorts int,PRIMARY KEY(SwitchID,ISCID))")
+"""
+
+"""
+AddMlagPortInstance
+
+(SwitchID integer,ISCID integer,PortID Text,MLAGID integer, \
+    VlanName Text,VlanTag integer,Tagged integer, \
+    PRIMARY KEY(SwitchID,MLAGID,VlanTag),FOREIGN KEY (ISCID) REFERENCES MLAGPeer(ISCID), FOREIGN KEY(SwitchID) REFERENCES MLAGPeer(SwitchID))")
+"""
+
+# Connect to the switches
+
+connect.ConnecttoSwitches()
+SwitchID_handler=connect.SwitchID_handlerdict
+for key in SwitchID_handler:
+    #-- AddMLAGPeerInstance
+    MLAGPeer.get_mlag_peer(SwitchID_handler[key],key)
+    MLAGSQL.DebugShowMLAGTable()
+    #-- AddMLAGPortInstance
+    MLAGPorts.MlagPort(SwitchID_handler[key],key)
+    MLAGSQL.DebugShowPortTable()
+connect.Closeconnectiontoswitches()
+
+#Debug functions to see  table content
+
+MLAGSQL.DebugShowPortTable()
+
+#Loading the Tables
+# PArse the customer data and get connection detail for each switch
+# Connect to each switch. For each switch, - DUrga
+# Get the MLAG Peer and MLAG port info and load the MLAGPeer Table - Ramesh
+# Get list of ports from the show MLAG port command
+# For each port in the list get the associated vlan Information - Name,tag,Link State etc.
+# For ISC port get the associated Vlan Information and load it into the DB
+
+#Notes : Looks like there will be a flag needed in PortInfo for quick look up of associated VLans
+#Validation Functions
+#GetList of all MLAG Ports for a given MLAGID in a Switch
+#for each Mlag Port get a list of associated Vlan tags
+#Get list of Vlan tags for the ISC Vlan
+#Check if each tag is represented in the ISC Vlan
+
+# GetMLAGParameters
+# Get specific parameters from the MLAG table needed like ISC VLan name / Tag , ISC IP ,Auth Method etc.
+
+#Delete all the tables
+MLAGSQL.DeleteTables()
+
+#Disconnect the database
+MLAGSQL.CloseDatabase()
