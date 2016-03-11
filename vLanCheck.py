87f5a2d4c4e82d83420a694c115cfdc80387d1f1
lkuppan <k.luxman@gmail.com>
2016-03-11 19:44:47 +0530

    Updated the script to show MLAG and ISC Port IDs

--- vLanCheck.py
+++ vLanCheck.py
@@ -1,8 +1,6 @@
 import MLAGSQL
 import logging
-import launch
 logging.basicConfig(level=logging.INFO)
-import sqlite3
 import sys
 
 
@@ -12,18 +10,18 @@ def CompareVlansBetween(ISCPortVLAN, MLAGPortVLAN):
     MLAGPortVLAN.sort()
 
     if MLAGPortVLAN == ISCPortVLAN:
-        print("PASS: ISC Port VLAN & MLAG Port VLAN matched")
+        print("\tPASS: ISC Port VLAN & MLAG Port VLAN matched")
     else:
-        #print("FAIL: ISC Port VLAN & MLAG Port VLAN mis-match")
-        print("FAIL:")
+        print("\tFAIL: ISC Port VLAN & MLAG Port VLAN mis-match")
         mlag_vlan=set(ISCPortVLAN).difference(MLAGPortVLAN)
         isc_vlan =set(MLAGPortVLAN).difference(ISCPortVLAN)
         if mlag_vlan:
-            sys.stdout.write ("MLAG Port is not associated with the VLAN(s):")
+            sys.stdout.write ("\tMLAG Port is not associated with the VLAN ")
             print (mlag_vlan)
         if isc_vlan:
-            sys.stdout.write ("ISC Port is not associated with the VLAN(s):")
+            sys.stdout.write ("\tISC Port is not associated with the VLAN ")
             print (isc_vlan)
+    print ("\n")
 
 def VlanCheck():
 
@@ -32,28 +30,48 @@ def VlanCheck():
     ISCPortVLAN=[]
     MLAGPortVLAN=[]
 
+    ''' Loop-1: Get SWID from the table '''
     GetSwId=c.execute("SELECT DISTINCT SwitchID FROM PortInfo")
     for SwID in GetSwId.fetchall():
         SwID=SwID[0]
-        print("\nPEER%s: VLAN Check Between ISC and MLAG ID" % SwID)
-        ''' Get ISCID from the Switch '''
-        result=c.execute("SELECT DISTINCT ISCID FROM PortInfo WHERE SwitchID=%s;" %(SwID))
-        ''' Get ISC Port VlanTag List '''
+        print("\n[] Diag: PEER%s VLAN Check Between ISC and MLAG ID" % SwID)
+
+        ''' Loop-1.1: Go by ISC ID from Table '''
+        result=c.execute("SELECT DISTINCT ISCID FROM PortInfo WHERE SwitchID='%s';" %(SwID))
         for isc_id in result.fetchall():
-            res=c.execute("SELECT VlanTag FROM PortInfo WHERE SwitchID=%s AND ISCID=%s AND MLAGID=0;" % (SwID, isc_id[0]))
-            for val in res.fetchall():
-                ISCPortVLAN.append(val[0])
+            ''' Loop-1.1.1: Get ISC Port ID from table '''
+            res=c.execute("SELECT DISTINCT PortID FROM PortInfo WHERE SwitchID='%s' AND ISCID='%s' AND MLAGID=0;" % (SwID, isc_id[0]))
+            for isc_pid in res.fetchall():
 
-                ''' Get List of MLAGID for each ISC '''
-                res=c.execute("SELECT DISTINCT MLAGID FROM PortInfo WHERE SwitchID=%s AND ISCID=%s AND MLAGID !='0';" %(SwID, isc_id[0]))
-                ''' Get VLAN List for each MLAG ID '''
-                for mlag_id in res.fetchall():
-                    res=c.execute("SELECT VlanTag FROM PortInfo WHERE SwitchID=%s AND ISCID=%s AND MLAGID='%s';" %(SwID, isc_id[0], mlag_id[0]))
-                    for val in res.fetchall():
-                        MLAGPortVLAN.append(val[0])
+                ''' Loop-1.1.1.1: populate ISCPortVLAN List'''
+                vlantag=c.execute("SELECT VlanTag FROM PortInfo WHERE SwitchID='%s' AND ISCID='%s' AND MLAGID=0 AND PortID='%s';" % (SwID, isc_id[0], isc_pid[0]))
+                for vlan in vlantag.fetchall():
+                    ISCPortVLAN.append(vlan[0])
+
+                ''' Loop-1.1.1.2: Get MLAG Port ID '''
+                mlag=c.execute("SELECT DISTINCT PortID FROM PortInfo WHERE SwitchID='%s' AND ISCID='%s' AND MLAGID !='0';" %(SwID, isc_id[0]))
+                for mlag_pid in mlag.fetchall():
+
+                    ''' Loop-1.1.1.2.1: Populate MLAGPortVLAN List '''
+                    mlag_vlan=c.execute("SELECT VlanTag FROM PortInfo WHERE SwitchID='%s' AND ISCID='%s' AND PortID='%s';" %(SwID, isc_id[0], mlag_pid[0]))
+                    for mlag_vid in mlag_vlan.fetchall():
+                        MLAGPortVLAN.append(mlag_vid[0])
+
+                    id=c.execute("SELECT DISTINCT MLAGID FROM PortInfo WHERE SwitchID='%s' AND ISCID='%s' AND PortID='%s';" %(SwID, isc_id[0], mlag_pid[0]))
+                    mlag_id=id.fetchall()[0]
+
                     ''' Compare ISC VLAN LIST with Each MLAG ID VLAN LIST '''
-                    print("\nTEST: Check VLAN between ISC ID : %s & MLAG ID: %s" %(isc_id[0], mlag_id[0]))
+                    print("\tTEST: Check VLAN between ISC Port : %s & MLAGID: %s, MLAG Port: %s" %(isc_pid[0], mlag_id[0], mlag_pid[0]))
                     CompareVlansBetween(ISCPortVLAN, MLAGPortVLAN)
-                    MLAGPortVLAN.clear()
-                ISCPortVLAN.clear()
 
+                    ''' Clear MLAGPortVLAN List and go to Loop-1.1.1.2 '''
+                    try:
+                        MLAGPortVLAN.clear()
+                    except:
+                        MLAGPortVLAN=[]
+
+                ''' Clear ISCPortVLAN list and go to Loop-1.1.1 '''
+                try:
+                    ISCPortVLAN.clear()
+                except:
+                    ISCPortVLAN=[]
